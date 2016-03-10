19e6d83ac99277bc546e164506fff4d0ec290f23
GastroBlastro <gibsonmacmillan@outlook.com>
2016-03-10 10:46:54 -0400

    Simon

---
+++ Simon/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Simon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.greenButton = new System.Windows.Forms.Button();
+            this.blueButton = new System.Windows.Forms.Button();
+            this.yellowButton = new System.Windows.Forms.Button();
+            this.redButton = new System.Windows.Forms.Button();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.historyLabel = new System.Windows.Forms.Label();
+            this.screenLabel = new System.Windows.Forms.Label();
+            this.lightBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.lightBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // greenButton
+            // 
+            this.greenButton.BackColor = System.Drawing.Color.Green;
+            this.greenButton.Enabled = false;
+            this.greenButton.Location = new System.Drawing.Point(12, 12);
+            this.greenButton.Name = "greenButton";
+            this.greenButton.Size = new System.Drawing.Size(196, 194);
+            this.greenButton.TabIndex = 0;
+            this.greenButton.UseVisualStyleBackColor = false;
+            this.greenButton.Click += new System.EventHandler(this.greenButtonMethod);
+            // 
+            // blueButton
+            // 
+            this.blueButton.BackColor = System.Drawing.Color.Blue;
+            this.blueButton.Enabled = false;
+            this.blueButton.Location = new System.Drawing.Point(229, 234);
+            this.blueButton.Name = "blueButton";
+            this.blueButton.Size = new System.Drawing.Size(197, 202);
+            this.blueButton.TabIndex = 1;
+            this.blueButton.UseVisualStyleBackColor = false;
+            this.blueButton.Click += new System.EventHandler(this.blueButtonMethod);
+            // 
+            // yellowButton
+            // 
+            this.yellowButton.BackColor = System.Drawing.Color.Gold;
+            this.yellowButton.Enabled = false;
+            this.yellowButton.Location = new System.Drawing.Point(12, 234);
+            this.yellowButton.Name = "yellowButton";
+            this.yellowButton.Size = new System.Drawing.Size(196, 202);
+            this.yellowButton.TabIndex = 2;
+            this.yellowButton.UseVisualStyleBackColor = false;
+            this.yellowButton.Click += new System.EventHandler(this.yellowButtonMethod);
+            // 
+            // redButton
+            // 
+            this.redButton.BackColor = System.Drawing.Color.Red;
+            this.redButton.Enabled = false;
+            this.redButton.Location = new System.Drawing.Point(229, 12);
+            this.redButton.Name = "redButton";
+            this.redButton.Size = new System.Drawing.Size(197, 194);
+            this.redButton.TabIndex = 3;
+            this.redButton.UseVisualStyleBackColor = false;
+            this.redButton.Click += new System.EventHandler(this.redButtonMethod);
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.newGameButton.ForeColor = System.Drawing.Color.White;
+            this.newGameButton.Location = new System.Drawing.Point(32, 454);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(108, 30);
+            this.newGameButton.TabIndex = 4;
+            this.newGameButton.Text = "New Game";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.ForeColor = System.Drawing.Color.White;
+            this.exitButton.Location = new System.Drawing.Point(299, 454);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(108, 30);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit Game";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // historyLabel
+            // 
+            this.historyLabel.AutoSize = true;
+            this.historyLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.historyLabel.Location = new System.Drawing.Point(391, 485);
+            this.historyLabel.Name = "historyLabel";
+            this.historyLabel.Size = new System.Drawing.Size(0, 13);
+            this.historyLabel.TabIndex = 7;
+            // 
+            // screenLabel
+            // 
+            this.screenLabel.Font = new System.Drawing.Font("Quartz MS", 18.75F, System.Drawing.FontStyle.Bold);
+            this.screenLabel.Image = global::Simon.Properties.Resources.screen;
+            this.screenLabel.Location = new System.Drawing.Point(229, 450);
+            this.screenLabel.Name = "screenLabel";
+            this.screenLabel.Size = new System.Drawing.Size(48, 45);
+            this.screenLabel.TabIndex = 8;
+            this.screenLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lightBox
+            // 
+            this.lightBox.Image = ((System.Drawing.Image)(resources.GetObject("lightBox.Image")));
+            this.lightBox.Location = new System.Drawing.Point(162, 450);
+            this.lightBox.Name = "lightBox";
+            this.lightBox.Size = new System.Drawing.Size(46, 45);
+            this.lightBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.lightBox.TabIndex = 6;
+            this.lightBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(438, 507);
+            this.Controls.Add(this.screenLabel);
+            this.Controls.Add(this.historyLabel);
+            this.Controls.Add(this.lightBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.redButton);
+            this.Controls.Add(this.yellowButton);
+            this.Controls.Add(this.blueButton);
+            this.Controls.Add(this.greenButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.lightBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button greenButton;
+        private System.Windows.Forms.Button blueButton;
+        private System.Windows.Forms.Button yellowButton;
+        private System.Windows.Forms.Button redButton;
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.PictureBox lightBox;
+        private System.Windows.Forms.Label historyLabel;
+        private System.Windows.Forms.Label screenLabel;
+    }
+}
+
---
+++ Simon/Form1.cs
@@ -0,0 +1,277 @@
+﻿//Simon Game Simulator
+//By: Gibson J. MacMillan
+//03.10.2014
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Simon
+{
+    public partial class Form1 : Form
+    {
+        //create necessary variables and assign classes
+        Random rand = new Random();
+        List<int> buttonHistory = new List<int>();
+        int counter, score;
+        bool comp;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //set score to 0 when program starts
+            score = 0;
+        }
+
+        /// <summary>
+        /// The computer takes turn clicking buttons while storing them in a list
+        /// </summary>
+        void compTurn()
+        {
+            //makes light red
+            lightBox.Image = Properties.Resources.red_on;
+
+            //adds computer choice to buttonHistory list for future reference
+            buttonHistory.Add(rand.Next(0, 4));
+
+            //depending on computer choice (from buttonHistory), clicks buttons for player to see
+            for (int i = 0; i < buttonHistory.Count(); i++)
+            {
+                if (buttonHistory[i] == 0)
+                {
+                    //runs greenButton method
+                    greenButton.PerformClick();
+                }
+                else if (buttonHistory[i] == 1)
+                {
+                    //runs redButton method
+                    redButton.PerformClick();
+                }
+                else if (buttonHistory[i] == 2)
+                {
+                    //runs yellowButton method
+                    yellowButton.PerformClick();
+                }
+                else if (buttonHistory[i] == 3)
+                {
+                    //runs blueButton method
+                    blueButton.PerformClick();
+                }
+
+                //if gone through all of computers choices, computer turn done
+                if (i + 1 == buttonHistory.Count())
+                {
+                    counter = 0;
+                    comp = false;
+                }
+            }
+
+            //set light green
+            lightBox.Image = Properties.Resources.green_on;
+
+
+        }
+
+        /// <summary>
+        /// Contains methods for all buttons visible
+        /// </summary>
+        #region buttons
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //closes program on exit button click
+            Close();
+        }
+
+        /// <summary>
+        /// prepares game for 1st round
+        /// </summary>
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            //sets button colours
+            greenButton.BackColor = Color.Green;
+            redButton.BackColor = Color.Red;
+            yellowButton.BackColor = Color.Gold;
+            blueButton.BackColor = Color.Blue;
+
+            //clears computer button history
+            buttonHistory.Clear();
+
+           //resets score and counter to 0
+            score = 0;
+            counter = 0;
+            screenLabel.Text = score.ToString("00");
+
+            //set computer turn to true
+            comp = true;
+
+            //enables buttons
+            greenButton.Enabled = true;
+            redButton.Enabled = true;
+            yellowButton.Enabled = true;
+            blueButton.Enabled = true;
+
+            //start computer turn
+            compTurn();
+        }
+        
+        void greenButtonMethod(object sender, EventArgs e)
+        {
+            if (buttonHistory[counter] == 0)
+            {
+                //runs moo sound, brightens colour, and after 1.5 secs resets colour and increases counter
+                SoundPlayer player = new SoundPlayer(Properties.Resources.moo);
+                player.Play();
+                greenButton.BackColor = Color.LimeGreen;
+                Refresh();
+                Thread.Sleep(1500);
+                greenButton.BackColor = Color.Green;
+                player.Stop();
+                counter++;
+            }
+
+            //if wrong button hit, runs wrongButton method
+           else
+            {
+                wrongButton();
+            }            
+
+            //if players turn done, increases score and goes to computer turn
+            if (counter == buttonHistory.Count() && comp == false)
+            {
+                score++;
+                screenLabel.Text = score.ToString("00"); 
+                counter = 0; 
+                comp = true;
+                compTurn();
+            }
+            
+        }
+
+        void redButtonMethod(object sender, EventArgs e)
+        {
+            //runs gaggle sound, brightens colour, and after 1.5 secs resets colour and increases counter
+            if (buttonHistory[counter] == 1)
+            {
+                SoundPlayer player = new SoundPlayer(Properties.Resources.goose);
+                player.Play();
+                redButton.BackColor = Color.Tomato;
+                Refresh();
+                Thread.Sleep(1000);
+                redButton.BackColor = Color.Red;
+                player.Stop();
+                counter++;
+                
+            }
+
+            //if wrong button hit, runs wrongButton method
+            else
+            {
+                wrongButton();
+            }
+
+            //if players turn done, increases score and goes to computer turn
+            if (counter == buttonHistory.Count() && comp == false)
+            {
+                score++;
+                screenLabel.Text = score.ToString("00");
+                counter = 0;
+                comp = true;
+                compTurn();
+            }
+        }
+
+        void yellowButtonMethod(object sender, EventArgs e)
+        {
+            //runs elephant sound, brightens colour, and after 1.5 secs resets colour and increases counter
+            if (buttonHistory[counter] == 2)
+            {
+                SoundPlayer player = new SoundPlayer(Properties.Resources.elephant);
+                player.Play();
+                yellowButton.BackColor = Color.Yellow;
+                Refresh();
+                Thread.Sleep(1500);
+                yellowButton.BackColor = Color.Gold;
+                player.Stop();
+                counter++;
+            }
+
+            //if wrong button hit, runs wrongButton method
+            else
+            {
+                wrongButton();
+            }
+            //if players turn done, increases score and goes to computer turn
+            if (counter == buttonHistory.Count() && comp == false)
+            {
+                score++;
+                screenLabel.Text = score.ToString("00");
+                counter = 0;
+                comp = true;
+                compTurn();
+            }
+        }
+
+        void blueButtonMethod(object sender, EventArgs e)
+        {
+            //runs hee-haw sound, brightens colour, and after 1.5 secs resets colour and increases counter
+            if (buttonHistory[counter] == 3)
+            {
+                SoundPlayer player = new SoundPlayer(Properties.Resources.donkey);
+                player.Play();
+                blueButton.BackColor = Color.DodgerBlue;
+                Refresh();
+                Thread.Sleep(1400);
+                blueButton.BackColor = Color.Blue;
+                player.Stop();
+                counter++;
+
+            }
+            //if wrong button hit, runs wrongButton method
+            else
+            {
+                wrongButton();
+            }
+            //if players turn done, increases score and goes to computer turn
+            if(counter == buttonHistory.Count() && comp == false)
+            {
+                score++;
+                screenLabel.Text = score.ToString("00");
+                counter = 0;
+                comp = true;
+                compTurn();
+            }
+        }
+
+        /// <summary>
+        /// Occurs when wrong button clicked by user, 
+        /// disables further actions by user until new game button clicked.
+        /// </summary>
+        public void wrongButton()
+        {
+            //plays fart noise, disabled and sets button to black, turns light off
+            SoundPlayer player = new SoundPlayer(Properties.Resources.end);
+            player.Play();
+            greenButton.BackColor = Color.Black;
+            redButton.BackColor = Color.Black;
+            yellowButton.BackColor = Color.Black;
+            blueButton.BackColor = Color.Black;
+            screenLabel.Text = score.ToString("00");
+            lightBox.Image = Properties.Resources.light_off;
+            greenButton.Enabled = false;
+            redButton.Enabled = false;
+            yellowButton.Enabled = false;
+            blueButton.Enabled = false;
+
+        }
+        #endregion
+    }
+}
